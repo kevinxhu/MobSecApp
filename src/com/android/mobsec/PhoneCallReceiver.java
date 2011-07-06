eca52eacdd25779db12f057daee3982724f521b9
unknown <kevin@.(none)>
2011-07-06 22:50:10 +0800

    Suppport to intercept incoming phonecall and end it anyway.
    Next it to support white list/blacklist filer for incoming call intercept.

---
+++ src/com/android/mobsec/PhoneCallReceiver.java
@@ -0,0 +1,41 @@
+package com.android.mobsec;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
+
+import com.android.internal.telephony.ITelephony;
+
+public class PhoneCallReceiver extends BroadcastReceiver {
+	private static final String TAG = "THIRI THE WUT YEE";
+	private ITelephony telephonyService;
+	@Override
+    public void onReceive(Context context, Intent intent) {
+		
+        Log.v(TAG, "Receving....");
+        TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);  
+        try 
+        {
+            Class c = Class.forName(telephony.getClass().getName());
+            Method m = c.getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+            telephonyService = (ITelephony) m.invoke(telephony);
+
+            Toast tag = Toast.makeText(context, "Call is not allowed in the meeting!!!", Toast.LENGTH_LONG);
+            tag.setDuration(25);
+            tag.show();
+            
+            telephonyService.silenceRinger();
+            telephonyService.endCall();
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }	
+}
