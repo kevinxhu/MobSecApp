2515c62bff47b118e8b033118f42d4bd591fb197
unknown <kevin@.(none)>
2011-07-07 21:44:44 +0800

    get incoming phone number.

--- src/com/android/mobsec/PhoneCallReceiver.java
+++ src/com/android/mobsec/PhoneCallReceiver.java
@@ -3,6 +3,7 @@ package com.android.mobsec;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -21,7 +22,8 @@ public class PhoneCallReceiver extends BroadcastReceiver {
         TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);  
         try 
         {
-            Class c = Class.forName(telephony.getClass().getName());
+            @SuppressWarnings("rawtypes")
+			Class c = Class.forName(telephony.getClass().getName());
             Method m = c.getDeclaredMethod("getITelephony");
             m.setAccessible(true);
             telephonyService = (ITelephony) m.invoke(telephony);
@@ -29,6 +31,10 @@ public class PhoneCallReceiver extends BroadcastReceiver {
             Toast tag = Toast.makeText(context, "Call is not allowed in the meeting!!!", Toast.LENGTH_LONG);
             tag.setDuration(25);
             tag.show();
+            
+            Bundle bundle = intent.getExtras();    
+            String phoneNr= bundle.getString("incoming_number");        
+            Log.v(TAG, "phoneNr: "+phoneNr);
             
             telephonyService.silenceRinger();
             telephonyService.endCall();
